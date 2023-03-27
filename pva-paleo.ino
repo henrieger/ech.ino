7d33c61dc2d8c1fff12008e8d3e9cee44d39616f
Henrique Rieger <henriquerieger2001@gmail.com>
2023-03-27 12:56:52 -0300

    [add] medição de temperatura

--- pva-paleo.ino
+++ pva-paleo.ino
@@ -1,6 +1,28 @@
+# include <LiquidCrystal.h>
+# include <DallasTemperature.h>
+# include <OneWire.h>
 
-void setup() {
+# define _1W_DATA 13
+
+OneWire oneWire(_1W_DATA);
+DallasTemperature sensors(&oneWire);
+
+void setup()
+{
+    Serial.begin(9600);
+
+    sensors.begin();
+    sensors.setResolution(12);
 }
 
-void loop() {
+void loop()
+{
+    sensors.requestTemperatures();
+    float temp = sensors.getTempCByIndex(0);
+
+    Serial.print("Temperatura atual: ");
+    Serial.print(temp);
+    Serial.println(" ºC");
+
+    delay(1000);
 }
