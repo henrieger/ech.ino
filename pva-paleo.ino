df42b3f2293ece751282ae200553e40b05734a8f
Henrique Rieger <henriquerieger2001@gmail.com>
2023-03-28 10:27:41 -0300

    [add] configs basicas para sensor de agua

--- pva-paleo.ino
+++ pva-paleo.ino
@@ -3,9 +3,14 @@
 
 # define _1W_DATA 13
 # define WATER_PIN A0
+# define BUTTON_PIN A1
+# define POTENTIOMETER_PIN A2
+# define BUZZER_PIN A3
 
 OneWire oneWire(_1W_DATA);
 DallasTemperature sensors(&oneWire);
+
+int waterBaseLevel;
 
 void setup()
 {
@@ -15,6 +20,11 @@ void setup()
     sensors.setResolution(12);
 
     pinMode(WATER_PIN, INPUT);
+    pinMode(BUTTON_PIN, INPUT);
+
+    Serial.println("Aperte o botao para configurar o nivel de agua");
+    while (digitalRead(BUTTON_PIN) == LOW) {}
+    waterBaseLevel = analogRead(WATER_PIN);
 }
 
 void loop()
@@ -28,9 +38,20 @@ void loop()
 
     int waterInput = analogRead(WATER_PIN);
     Serial.print("Nível da água: ");
-    Serial.println(waterInput);
+    Serial.println(waterBaseLevel - waterInput);
+
+    int waterThreshold = analogRead(POTENTIOMETER_PIN) / 100;
+    Serial.print("Threshold: ");
+    Serial.println(waterThreshold);
+    
+    if (abs(waterBaseLevel - waterInput) > waterThreshold)
+    {
+        delay(10);
+        tone(BUZZER_PIN, 440);
+        delay(100);
+        noTone(BUZZER_PIN);
+    }
 
     Serial.println();
-
     delay(1000);
 }
